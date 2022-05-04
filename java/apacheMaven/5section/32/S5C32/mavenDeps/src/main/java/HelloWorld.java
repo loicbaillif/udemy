7f8753f3ac37169560d99ee9287fff2269a76e45
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-04 20:52:20 +0200

    Update HelloWorld.java

--- java/apacheMaven/5section/32/S5C32/mavenDeps/src/main/java/HelloWorld.java
+++ java/apacheMaven/5section/32/S5C32/mavenDeps/src/main/java/HelloWorld.java
@@ -1,6 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World?");
-        System.out.println();
+        System.out.println(StringUtils.abbreviate("Hello, World", 4));
     }
 }

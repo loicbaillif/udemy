eccb8fce0806d8198df8cf7822e6016aa3cd0109
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-08 18:07:29 +0200

    S6C43/final

--- java/apacheMaven/6section/43/S6C43/src/test/java/TestWithMaven.java
+++ java/apacheMaven/6section/43/S6C43/src/test/java/TestWithMaven.java
@@ -1,6 +1,7 @@
 public class TestWithMaven {
 
     public void testOtherClass() {
+        // This class and this method will also be executed during test phase.
         System.out.println("TEST - From TestWithMaven class");
     }
 }

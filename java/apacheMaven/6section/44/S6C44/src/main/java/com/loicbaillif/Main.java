50b42af3e5277d02620ccba72f86c45645022bfd
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-08 18:59:15 +0200

    S6C44/final

--- java/apacheMaven/6section/44/S6C44/src/main/java/com/loicbaillif/Main.java
+++ java/apacheMaven/6section/44/S6C44/src/main/java/com/loicbaillif/Main.java
@@ -2,6 +2,8 @@ package com.loicbaillif;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("To launch this from executable JAR, please use");
+        System.out.println("java -jar S6C44-1.0-SNAPSHOT.jar");
         System.out.println("Hello world!");
     }
 }

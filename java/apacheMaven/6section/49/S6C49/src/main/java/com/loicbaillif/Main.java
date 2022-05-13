9713b4fae4538759a3c76865987e3eee07588c87
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-13 17:44:48 +0200

    S6C49/init
    Common Maven commands and cheat sheet

---
+++ java/apacheMaven/6section/49/S6C49/src/main/java/com/loicbaillif/Main.java
@@ -0,0 +1,16 @@
+package com.loicbaillif;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.center(
+                " Apache Maven: Beginner to Guru ",
+                70,
+                '*'));
+        System.out.println(StringUtils.center(
+                " S6C49 - Common Maven Commands and cheat sheet ",
+                70,
+                '*'));
+    }
+}

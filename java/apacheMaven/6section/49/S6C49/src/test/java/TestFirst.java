233fe99dce2511ba82d9f97069d0aa53fc3a6010
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-13 17:55:38 +0200

    S6C49/ft
    Cheat Sheet resource added;
    TestFirst class created (2 functions)

---
+++ java/apacheMaven/6section/49/S6C49/src/test/java/TestFirst.java
@@ -0,0 +1,18 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class TestFirst {
+    public void testHello() {
+        System.out.println(StringUtils.center(
+                " TestFirst class - testHello ",
+                60,
+                '#'
+        ));
+    }
+
+    public void testWorld() {
+        System.out.println(StringUtils.center(
+                " TestFirst class - testWorld ",
+                60,
+                '#'
+        ));
+}

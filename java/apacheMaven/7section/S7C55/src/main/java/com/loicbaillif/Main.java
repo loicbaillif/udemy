01f2b2dee8101599875f8126b479ff9bfd83f469
loicbaillif <61039417+loicbaillif@users.noreply.github.com>
2022-05-14 18:38:59 +0200

    S7C55/init
    Using Maven to generate Java Classes from JSON Schema

---
+++ java/apacheMaven/7section/S7C55/src/main/java/com/loicbaillif/Main.java
@@ -0,0 +1,7 @@
+package com.loicbaillif;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("S7C55 - Maven JSON Schema Example");
+    }
+}
---
+++ java/apacheMaven/7section/S7C55/src/test/java/repoStructure.md
@@ -0,0 +1,2 @@
+# Repository Structure File
+The purpose of this file is solely to save project entire repository to Git Hub

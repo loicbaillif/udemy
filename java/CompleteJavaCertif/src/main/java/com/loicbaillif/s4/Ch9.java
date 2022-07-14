555807db623749ba754d15241d28f5a358e0a67f
loicbaillif <larmedeciel@gmail.com>
2022-07-15 01:13:05 +0200

    CompJavaCert/s4/c9/init
    S4-Ch9 : Variables assignment, first int

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s4/Ch9.java
@@ -0,0 +1,17 @@
+package com.loicbaillif.s4;
+
+import com.loicbaillif.format.Print;
+
+public class Ch9 {
+    public static void main() {
+        Print.title(" Section 4 - Chapter 9 : Variables and Data Types ",
+                (short) 80, '*', ' ');
+        // Variable assignment, Integers
+        int x;
+        x = 4;
+        System.out.println(x);
+        x = 23;
+        System.out.println(x);
+
+    }
+}

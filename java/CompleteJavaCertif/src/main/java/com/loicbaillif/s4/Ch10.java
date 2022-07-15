2fb7757d1d3c96df4fd4589b7990e9a02bc49165
loicbaillif <larmedeciel@gmail.com>
2022-07-15 20:31:51 +0200

    CompJavaCert/s4/c10/v1
    Integers data taypes

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s4/Ch10.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s4/Ch10.java
@@ -6,5 +6,11 @@ public class Ch10 {
     public static void main() {
         Print.title(" Section 4 - Chapter 10: More on data types ",
                 (short) 80, '*');
+        System.out.println("int range: Integer coded on 4 bytes.");
+        System.out.println("byte range: Integer coded on 1 byte.");
+        System.out.println("short range: Integer coded on 2 bytes");
+        System.out.println("long range: Integer coded on 8 bytes");
+        long bigNumber = 100000000000L;
+        System.out.println("long bigNumber = 10000000000L");
     }
 }

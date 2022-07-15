3435d59d523b6cdea689ddc487d360308653e699
loicbaillif <larmedeciel@gmail.com>
2022-07-15 20:35:30 +0200

    CompJavaCert/s4/c10/v1
    Specification on integer values
    Next: other primitive types

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s4/Ch10.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s4/Ch10.java
@@ -8,7 +8,9 @@ public class Ch10 {
                 (short) 80, '*');
         System.out.println("int range: Integer coded on 4 bytes.");
         System.out.println("byte range: Integer coded on 1 byte.");
+        System.out.println("\tbyte min = -128\n\tbyte max = 127");
         System.out.println("short range: Integer coded on 2 bytes");
+        System.out.println("\tshort min = -32768\n\tshort max = 32767");
         System.out.println("long range: Integer coded on 8 bytes");
         long bigNumber = 100000000000L;
         System.out.println("long bigNumber = 10000000000L");

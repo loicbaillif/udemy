223a8ef0ffe302f80e1c17e511519e13d4784b33
loicbaillif <larmedeciel@gmail.com>
2022-07-17 22:13:07 +0200

    CompJavaCert/s4/c11/init
    Arrays
    Chapter initialization

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/Main.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/Main.java
@@ -1,8 +1,7 @@
 package com.loicbaillif;
 
 import com.loicbaillif.format.Print;
-import com.loicbaillif.s4.Ch10;
-import com.loicbaillif.s4.Ch9;
+import com.loicbaillif.s4.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +9,6 @@ public class Main {
                 (short) 80);
         Ch9.main();
         Ch10.main();
+        Ch11.main();
     }
 }
---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s4/Ch11.java
@@ -0,0 +1,11 @@
+package com.loicbaillif.s4;
+
+import com.loicbaillif.format.Print;
+
+public class Ch11 {
+    public static void main() {
+        Print.title(" Section 4 - Chapter 11: Working with Arrays ",
+                (short) 80, '*');
+        
+    }
+}

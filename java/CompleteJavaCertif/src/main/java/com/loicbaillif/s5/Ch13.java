14fa90da595ee7ce57d15f58a39ad601df546c35
loicbaillif <larmedeciel@gmail.com>
2022-08-01 20:55:35 +0200

    CompJavaCert/s5/c13/v3
    method updated to accept an argument, and tested.

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s5/Ch13.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s5/Ch13.java
@@ -7,10 +7,11 @@ public class Ch13 {
         Print.title(" Section 4 - Chapter 13: Methods in Java ",
                 (short) 80, '*');
         System.out.println("Invoking method println() to display this text");
-        printSomething();
+        printSomething("hammer");
     }
 
-    public static void printSomething() {
+    public static void printSomething(String argument) {
         System.out.println("Hi Universe!");
+        System.out.println("You asked for " + argument + "?");
     }
 }

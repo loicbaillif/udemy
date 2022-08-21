cf7a7a7b8c8339abaccfe20869d71404e5c1b47d
loicbaillif <larmedeciel@gmail.com>
2022-08-21 20:11:19 +0200

    CompJavaCert/s5/c15/v1
    Human class and its attributes created
    Next: create methods for this class

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Human.java
@@ -0,0 +1,11 @@
+package com.loicbaillif.s6;
+
+public class Human {
+    // Attributes
+    int age;
+    static int nbHumans;
+    int heightInCms;
+    String eyeColour;
+    String firstName;
+    String lastName;
+}

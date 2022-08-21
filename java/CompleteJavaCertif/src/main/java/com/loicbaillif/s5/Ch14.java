8dfcad4592f53bf40887cdcd0e7d878450493569
loicbaillif <larmedeciel@gmail.com>
2022-08-21 10:52:10 +0200

    CompJavaCert/s5/c14/v2
    return-type methods

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s5/Ch14.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s5/Ch14.java
@@ -1,5 +1,7 @@
 package com.loicbaillif.s5;
 
+import java.util.Scanner;
+
 import com.loicbaillif.format.Print;
 
 public class Ch14 {
@@ -7,8 +9,13 @@ public class Ch14 {
         Print.title(" Section 4 - Chapter 14: Methods visibility ",
                 (short) 80, '*');
 
+        // void-type methods
         noReturn1();
         noReturn2(33);
+
+        // Return-type methods
+        int userInput = getUserInt();
+        System.out.println(add10(userInput));
 
         Print.title(" End of Chapter 14 ", (short) 80, '*');
     }
@@ -21,5 +28,19 @@ public class Ch14 {
     public static void noReturn2(int age) {
         System.out.println("I don't need to return anything to wish you " +
                 "a happy " + age + " birthday!");
+    }
+
+    public static String add10(int userInput) {
+        return String.format("%d + 10 = %d", userInput, userInput + 10);
+    }
+
+    public static int getUserInt() {
+        System.out.println("Please enter an integer:\n");
+        Scanner scanner1 = new Scanner(System.in);
+        while (!scanner1.hasNextInt()) {
+            System.out.println("Please enter an integer:\n");
+            scanner1.next();
+        }
+        return scanner1.nextInt();
     }
 }

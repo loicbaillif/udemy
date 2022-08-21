994c88d49a49a68f19ceb47da13d64825b5560ac
loicbaillif <larmedeciel@gmail.com>
2022-08-21 10:13:10 +0200

    CompJavaCert/s5/c14/v1
    void methods
    Next: methods which return data

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s5/Ch14.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s5/Ch14.java
@@ -7,8 +7,19 @@ public class Ch14 {
         Print.title(" Section 4 - Chapter 14: Methods visibility ",
                 (short) 80, '*');
 
-
+        noReturn1();
+        noReturn2(33);
 
         Print.title(" End of Chapter 14 ", (short) 80, '*');
+    }
+
+    public static void noReturn1() {
+        System.out.println("This method does not return anything, even though " +
+                "it can print something.");
+    }
+
+    public static void noReturn2(int age) {
+        System.out.println("I don't need to return anything to wish you " +
+                "a happy " + age + " birthday!");
     }
 }

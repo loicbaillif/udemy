c845ad4586225b1044ce2a3ad0bc43d715582406
loicbaillif <larmedeciel@gmail.com>
2022-08-21 20:11:45 +0200

    CompJavaCert/s5/c15/v2
    Methods defined for Human
    Next: create instances

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Human.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Human.java
@@ -8,4 +8,25 @@ public class Human {
     String eyeColour;
     String firstName;
     String lastName;
+
+    // Methods
+    public void speak() {
+        System.out.printf("Hello, my name is %s %s", firstName, lastName);
+        System.out.printf("I am %.2f meters tall.", heightInCms / 100);
+        System.out.printf("I am %d years old.", age);
+        System.out.printf("My eyes are %s", eyeColour);
+        System.out.printf("Oh and I am the human number %d", nbHumans);
+    }
+
+    public void eat() {
+        System.out.println("I am eating ...");
+    }
+
+    public void walk() {
+        System.out.println("I am walking");
+    }
+
+    public void work() {
+        System.out.println("I am working");
+    }
 }

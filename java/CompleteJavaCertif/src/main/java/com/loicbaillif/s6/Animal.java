6627ac6d79065d8cd00912b16a50104f865156be
loicbaillif <larmedeciel@gmail.com>
2022-08-21 22:16:13 +0200

    CompJavaCert/s5/c16/v8
    Description texts updated
    End of chapter.

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Animal.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Animal.java
@@ -15,26 +15,28 @@ public class Animal {
         this.family = family;
         this.name = name;
         this.origin = origin;
-        System.out.printf("%s joined our zoo!", name);
+        System.out.printf("%s joined our zoo!%n", name);
     }
 
     // Methods
     public void angry() {
-        System.out.printf("%s is angry and becomes loud", name);
+        System.out.printf("%s is angry and becomes loud.%n", name);
     }
 
     public void compareOrigin(Animal animal2) {
         if (Objects.equals(this.origin, animal2.origin)) {
-            System.out.println("They come from the same region!");
-            System.out.println("Maybe they already knew each other");
+            System.out.printf("%ss and %ss come from the same region!%n",
+                    this.name, animal2.name);
+            System.out.println("Maybe they already knew each other...");
         } else {
-            System.out.println("They don't live in the same region");
+            System.out.printf("%ss and %ss don't live in the same region%n",
+                    this.name, animal2.name);
             System.out.println("Before being here they never imagined " +
                     "each other existence!");
         }
     }
 
     public void eat(String food) {
-        System.out.printf("Vets are feeding %s with %s", this.name, food);
+        System.out.printf("Vets are feeding %s with %s%n", this.name, food);
     }
 }

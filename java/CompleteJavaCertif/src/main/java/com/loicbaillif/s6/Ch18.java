5ad2aaee23aec6db7af566e6a6db998c75df2607
loicbaillif <larmedeciel@gmail.com>
2022-08-22 21:48:51 +0200

    CompJavaCert/s5/c18/v1
    The stack: calling methods from main;

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch18.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch18.java
@@ -8,7 +8,17 @@ public class Ch18 {
                 " Section 6 - Chapter 18: Java Stack + Heap ",
                 (short) 80);
 
+        int age;
+        age = 12;
+        System.out.println("Variable 'age' created. Local variable.");
+        doWork();
 
         Print.title(" End of chapter 18 ", (short) 80, '*');
+    }
+
+    public static void doWork() {
+        System.out.println("I am working.");
+        float weight = 75.4f;
+        System.out.println("weight = " + weight);
     }
 }

2c818c377912e566b19573daa85a793ff6edf189
loicbaillif <larmedeciel@gmail.com>
2022-08-22 22:09:33 +0200

    CompJavaCert/s5/c18/v3
    Variables by reference in heap
    End of chapter

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch18.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch18.java
@@ -17,10 +17,12 @@ public class Ch18 {
         Animal pet1 = new Animal(11, "Fishes", "goldfish", "Asia");
         // pet1 stored in stack will point to Animal() memory address in heap.
         System.out.println(pet1);
-        pet1 = new Animal(22, "Wolf", "mammal", "Europe");
+        pet1 = new Animal(22, "Mammals", "wolf", "Europe");
         // pet1 re-assigned: it points toward a new Animal() memory address in heap;
         // former pet1 (goldfish) is still in heap, but cannot be accessed anymore.
         // Garbage collector will take care of eliminating it.
+
+        Animal pet2 = new Animal(5, "Birds", "eagle", "America");
 
         Print.title(" End of chapter 18 ", (short) 80, '*');
     }

f0a7feebd8546db7dca90a815e10909bc84f9620
loicbaillif <larmedeciel@gmail.com>
2022-08-24 22:49:30 +0200

    CompJavaCert/s6/c19/v2
    New method added to class
    Next: create Fish class, child of Animal class

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Bird.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Bird.java
@@ -4,14 +4,23 @@ public class Bird extends Animal {
     // Attributes
     boolean flying;
 
+    // Constructor
     public Bird(
             int age,
             String family,
             String name,
             String origin,
-            boolean flying)
-    {
+            boolean flying) {
         super(age, family, name, origin);
         this.flying = flying;
+    }
+
+    // Methods
+    public void fly() {
+        if (flying) {
+            System.out.println("I believe I can fly!");
+        } else {
+            System.out.println("Boohoohoo nooo I can't");
+        }
     }
 }

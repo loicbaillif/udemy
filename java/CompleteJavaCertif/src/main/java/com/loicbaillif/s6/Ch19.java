0f8e7e672ff0969082dd927d368a89f7a532c994
loicbaillif <larmedeciel@gmail.com>
2022-08-24 22:36:50 +0200

    CompJavaCert/s6/c19/init
    Inheritance
    Initialization

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch19.java
@@ -0,0 +1,13 @@
+package com.loicbaillif.s6;
+
+import com.loicbaillif.format.Print;
+
+public class Ch19 {
+    public static void main() {
+        Print.title(" Section 6 - Chapter 19: Inheritance ", (short) 80);
+
+
+
+        Print.title(" End of chapter 19 ", (short) 80, '*');
+    }
+}

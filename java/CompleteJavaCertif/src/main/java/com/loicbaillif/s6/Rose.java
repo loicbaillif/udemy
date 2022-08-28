e71b3bcba5102da3770933091abed353aeda78cb
loicbaillif <larmedeciel@gmail.com>
2022-08-28 20:31:19 +0200

    CompJavaCert/s6/c22/v2
    Class Rose created, extends Plantae;
    Next: create an instance

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Rose.java
@@ -0,0 +1,8 @@
+package com.loicbaillif.s6;
+
+public class Rose extends Plantae{
+    @Override
+    public void describeFlowers() {
+        System.out.println("Great variety in colours, and beautiful.");
+    }
+}

fee57d51c96d5d4c391030a9a2a90d87b912a5fe
loicbaillif <larmedeciel@gmail.com>
2022-08-28 18:12:50 +0200

    CompJavaCert/s6/c21/init
    Chapter 21: Interfaces
    Initialization

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch21.java
@@ -0,0 +1,13 @@
+package com.loicbaillif.s6;
+
+import com.loicbaillif.format.Print;
+
+public class Ch21 {
+    public static void main() {
+        Print.title(" Section 6 - Chapter 21: Interfaces ", (short) 80);
+
+        
+
+        Print.title(" End of chapter 21 ", (short) 80, '*');
+    }
+}

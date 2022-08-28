0f8f0409859f53556a3c04bb9035c9d6ec4b1046
loicbaillif <larmedeciel@gmail.com>
2022-08-28 19:00:26 +0200

    CompJavaCert/s6/c21/v2
    Class Parrot created, for demo purpose

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Parrot.java
@@ -0,0 +1,8 @@
+package com.loicbaillif.s6;
+
+public class Parrot extends Bird {
+
+    public Parrot(int age, String family, String name, String origin, boolean flying) {
+        super(age, family, name, origin, flying);
+    }
+}

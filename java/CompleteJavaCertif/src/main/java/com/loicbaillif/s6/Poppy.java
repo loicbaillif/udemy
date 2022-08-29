1e0be87e19642a674d712500cef349fb6b092c3d
loicbaillif <larmedeciel@gmail.com>
2022-08-29 21:18:02 +0200

    CompJavaCert/s6/c22/v2
    Class Poppy (extending Plantae) created

---
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Poppy.java
@@ -0,0 +1,8 @@
+package com.loicbaillif.s6;
+
+public class Poppy extends Plantae {
+    @Override
+    public void describeFlowers() {
+        System.out.println("Delicate intense red flower");
+    }
+}

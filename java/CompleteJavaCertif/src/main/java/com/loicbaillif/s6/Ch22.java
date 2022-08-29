88c17a13f05e969faa8a45cbb651ec4487b4cdf2
loicbaillif <larmedeciel@gmail.com>
2022-08-29 21:18:24 +0200

    CompJavaCert/s6/c22/v3
    Poppy and Rose classes tested

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch22.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Ch22.java
@@ -6,7 +6,11 @@ public class Ch22 {
     public static void main() {
         Print.title(" Section 6 - Chapter 22: Abstract Classes ", (short) 80);
 
+        Rose rose1 = new Rose();
+        rose1.describeFlowers();
 
+        Poppy poppy1 = new Poppy();
+        poppy1.describeFlowers();
 
         Print.title(" End of chapter 22 ", (short) 80, '*');
     }

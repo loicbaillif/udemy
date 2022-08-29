9dc201b2aa18a2694067acc0ff7600fa352bd35d
loicbaillif <larmedeciel@gmail.com>
2022-08-29 21:30:24 +0200

    CompJavaCert/s6/c22/v4
    Added toString method (default specification for the time-being) to Plantae class

--- java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Plantae.java
+++ java/CompleteJavaCertif/src/main/java/com/loicbaillif/s6/Plantae.java
@@ -3,4 +3,9 @@ package com.loicbaillif.s6;
 public abstract class Plantae {
 
     public abstract void describeFlowers();
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
